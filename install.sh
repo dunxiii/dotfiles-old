adb8826dc86cf60ed017521c8cfc89aa0d93dc8e
Jack Söderblom <dunxiii@gmail.com>
2015-04-08 18:39:52 +0200

    Added userfriendly menu

--- install.sh
+++ install.sh
@@ -6,8 +6,39 @@
 function conf_i3 {
 	mkdir -p ~/.i3
 	ln -s ${PWD}/i3/config ~/.i3/config
+	return $?
 }
 
-conf_i3
+function conf_conky {
+	mkdir -p ~/.conky
+	ln -s ${PWD}/conky/.conky_statusbar ~/.conky/.conky_statusbar
+	ln -s ${PWD}/conky/.conky_init.sh ~/.conky/.conky_init.sh
+	return $?
+}
+
+
+PS3='Please enter your choice: '
+options=("i3" "conky" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "i3")
+	    conf_i3
+	    if (true); then
+            	echo "Configurationfiles for i3 was linked."
+	    fi
+            ;;
+        "conky")
+            conf_conky
+	    if (true); then
+	    	echo "Configurationfiles for conky was linked."
+            fi
+	    ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
 
 exit 0
