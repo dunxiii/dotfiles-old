2659306c8c882f384f46c1c701bf81d9b3398e32
Jack Söderblom <dunxiii@gmail.com>
2015-04-12 18:50:20 +0200

    Added vim support

--- install.sh
+++ install.sh
@@ -1,38 +1,56 @@
 #!/bin/bash
-##########################################################
-# Script to link the settings from git to the right place.
-##########################################################
+#####################################
+# Script to link files from git to ~/
+#####################################
 
 function conf_i3 {
+	# Make directory if there is none
 	mkdir -p ~/.i3
+
+	# Rename original files
+	[ -e ~/.i3/config] && mv ~/.i3/config ~/.i3/config.bak
+
+	# Link new files
 	ln -s ${PWD}/i3/config ~/.i3/config
-	return $?
 }
 
 function conf_conky {
+	# Make directory if there is none
 	mkdir -p ~/.conky
+
+	# Rename original files
+	[ -e ~/.conky/.conky_statusbar] && mv ~/.conky/.conky_statusbar ~/.conky/.conky_statusbar.bak
+	[ -e ~/.conky/.conky_init.sh] && mv ~/.conky/.conky_init.sh ~/.conky/.conky_init.sh.bak
+
+	# Link new files
 	ln -s ${PWD}/conky/.conky_statusbar ~/.conky/.conky_statusbar
 	ln -s ${PWD}/conky/.conky_init.sh ~/.conky/.conky_init.sh
-	return $?
 }
 
+function conf_vim {
+	# Make directory if there is none
+	#mkdir -p ~/.vim
+
+	# Rename original files
+	[ -e ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
+
+	# Link new files
+	ln -s ${PWD}/vim/.vimrc ~/.vimrc
+}
 
 PS3='Please enter your choice: '
-options=("i3" "conky" "Quit")
+options=("i3" "conky" "vim" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "i3")
 	    conf_i3
-	    if (true); then
-            	echo "Configurationfiles for i3 was linked."
-	    fi
             ;;
         "conky")
             conf_conky
-	    if (true); then
-	    	echo "Configurationfiles for conky was linked."
-            fi
+	    ;;
+        "vim")
+            conf_vim
 	    ;;
         "Quit")
             break
