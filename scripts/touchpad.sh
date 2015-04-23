97626fd9f3320c39e243ae72255e1499ff2ac655
Jack Söderblom <dunxiii@gmail.com>
2015-04-23 08:18:10 +0200

    Better alternative to disable touchpad

---
+++ scripts/touchpad.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ID=`xinput --list | grep -Eo 'Touchpad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+
+STATE=`xinput --list-props ${ID} | grep 'Device Enabled' | awk '{print $4}'`
+
+if [ ${STATE} -eq 1 ]
+then
+    xinput disable $ID
+else
+    xinput enable $ID
+fi
