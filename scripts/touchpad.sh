7e42337a145adf2fbe13393026db45fcd462d46c
Jack Söderblom <dunxiii@gmail.com>
2015-04-23 12:33:47 +0200

    Total rewrite

--- scripts/touchpad.sh
+++ scripts/touchpad.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-ID=`xinput --list | grep -Eo 'Touchpad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+# Get the device id for the touchpad
+device_id=`xinput --list | grep -Eo 'Touchpad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
 
-STATE=`xinput --list-props ${ID} | grep 'Device Enabled' | awk '{print $4}'`
+# Get the current state of the touchpad, 1 = on, 0 = off
+device_state=`xinput --list-props ${device_id} | grep 'Device Enabled' | awk '{print $4}'`
 
-if [ ${STATE} -eq 1 ]
-then
-    xinput disable $ID
-else
-    xinput enable $ID
+# If no argument is given, toggle
+[ ${device_state} -eq 1 ] && xinput disable ${device_id} || xinput enable ${device_id}
+
+# If state is given as argument
+if [ $# -gt 0 ]; then
+	# If argument is disable
+	if [ $1 == "disable" ]; then
+    		xinput disable ${device_id}
+
+	# If argument is enable
+	elif [ $1 == "enable" ]; then
+		xinput enable ${device_id}
+	fi
 fi
+exit 0
