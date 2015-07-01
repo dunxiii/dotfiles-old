39f22a8f2266f6286c07fc1e642d725adbdf43bb
dunxiii <dunxiii@gmail.com>
2015-07-01 12:39:25 +0200

    Update

--- i3/i3exit.sh
+++ i3/i3exit.sh
@@ -3,7 +3,6 @@
 function lock {
     # Dependencies:
     # imagemagick
-    # i3lock-color-git
     # scrot
 
     IMAGE=/tmp/i3lock.png
@@ -13,13 +12,14 @@ function lock {
 
     # List fonts with command "convert -list font"
 
+    # Take screenshot
     scrot $IMAGE
-    convert $IMAGE -level 0%,100%,0.6 -blur $BLURTYPE -font Inconsolata-for-Powerline -pointsize 26 -fill white -gravity center - | composite -gravity center lock.png - $IMAGE
-    i3lock --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c -i $IMAGE
 
-    #convert $IMAGE -level 0%,100%,0.6 -blur $BLURTYPE -font Inconsolata-for-Powerline -pointsize 26 -fill white -gravity center -annotate +0+200 'Type password to unlock' - | composite -gravity center lock.png - $IMAGE
-    #i3lock --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c  -i $IMAGE
+    # Add locksymbol to screenshot
+    convert $IMAGE -level 0%,100%,0.6 -blur $BLURTYPE -font Inconsolata-for-Powerline -pointsize 26 -fill white -gravity center -annotate +0+200 "$user" - | composite -gravity center /home/jack/.i3/lock.png - $IMAGE
 
+    # Lock computer with screenshot as background
+    i3lock -ui $IMAGE
     rm $IMAGE
 }
 
@@ -31,7 +31,8 @@ case "$1" in
         i3-msg exit
         ;;
     suspend)
-        lock && systemctl suspend
+        lock
+        systemctl suspend
         ;;
     reboot)
         systemctl reboot
