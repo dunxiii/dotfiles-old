6b82d6d693e448a2d68b776f1b3fe26ec3899fb3
Jack Söderblom <dunxiii@gmail.com>
2016-02-10 10:47:40 +0100

    Refacorted and added more comments

--- i3/i3exit-rofi.sh
+++ i3/i3exit-rofi.sh
@@ -3,15 +3,12 @@
 #-------------------------------------------------------------------------------
 # Author: Jack Söderblom
 # Description: Uses rofi as an power settings menu
-
-# Revision history:
-#   8 feb 2016 16:04:55
-#   Created script.
-
+#
 # If script is large make -h option
 # Use curly brackets arround variables
 # ------------------------------------------------------------------------------
 
+# Get user that started the visual environment
 user=$(w -sh | grep tty$(fgconsole 2>/dev/null) | awk '{ print $1 }')
 
 # If used from cmd directly set user var correctly
@@ -22,41 +19,43 @@ fi
 
 # Lock function
 lock() {
-    i3lock -i /home/$user/Pictures/wallpaper.png
+    i3lock -i /home/${user}/Pictures/wallpaper.png
 }
+
+# List items
+list="1) lock
+      2) logout
+      3) poweroff
+      4) reboot
+      5) reboot to UEFI
+      6) suspend"
 
 # Menu to print on screen
 # sed string removes leading whitespace only
 selection=$(
-    echo \
-        "1) lock
-        2) logout
-        3) poweroff
-        4) reboot
-        5) reboot to UEFI
-        6) suspend"\
+    echo -e "${list}" \
     | sed -e 's/^[[:space:]]*//' \
     | rofi -dmenu -auto-select -fullscreen -p "Power option:"
 )
 
 # What to execute
 case "${selection}" in
-    "1) lock")
+    "1)"*)
         $(lock)
         ;;
-    "2) logout")
+    "2)"*)
         $(i3-msg exit)
         ;;
-    "3) poweroff")
+    "3)"*)
         $(lock && systemctl suspend)
         ;;
-    "4) reboot")
+    "4)"*)
         $(systemctl reboot)
         ;;
-    "5) reboot to UEFI")
+    "5)"*)
         $(systemctl reboot --firmware-setup)
         ;;
-    "6) suspend")
+    "6)"*)
         $(systemctl poweroff)
         ;;
     *)
