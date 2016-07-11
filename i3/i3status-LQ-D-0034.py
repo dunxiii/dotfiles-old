7bf8f94b502c7cb0707be2f77239698d74a307cd
Jack Söderblom <dunxiii@gmail.com>
2016-07-11 22:32:47 +0200

    Reworked bar

--- i3/i3status-LQ-D-0034.py
+++ i3/i3status-LQ-D-0034.py
@@ -1,4 +1,5 @@
 from i3pystatus import Status
+from i3pystatus.updates import pacman
 
 """
 Python packet dependency:
@@ -10,26 +11,40 @@ psutil
 
 status = Status(standalone=True)
 
-status.register("clock", format=" %d %B %H:%M",hints={"color":"#ffffff"})
+status.register("clock",
+                format="%d %B %H:%M",
+                hints={"color":"#ffffff"},
+                on_leftclick="xdg-open https://calendar.google.com")
 
 status.register("pulseaudio",
-                format="   {volume}% ",
-                hints={"color":"#ffffff"})
+                format="  {volume}%",
+                hints={"color":"#ffffff"},
+                on_upscroll="zenbook-audio.sh -v +",
+                on_downscroll="zenbook-audio.sh -v -",
+                on_leftclick="zenbook-audio.sh -m",
+                on_rightclick="pavucontrol")
 
-status.register("mem", format=" MEM {percent_used_mem}%")
+status.register("mem",
+                format="MEM {percent_used_mem}%",
+                on_leftclick="terminator --geometry=600x400 -x htop")
 
 status.register("cpu_usage",
-                format= "CPU {usage}%",
+                format="CPU {usage}%",
                 hints={ "color":        "#ffffff",
                         "full_text":    "CPU 4%",
                         "min_width":    "CPU 100%",
                         "align":        "center"
-                })
+                },
+                on_leftclick="terminator --geometry=600x400 -x htop")
 
 status.register("network",
         interface="eno1",
-        format_up=" {v4cidr} ",
+        format_up="{v4cidr}",
         format_down="")
+
+status.register("updates",
+        backends = [pacman.Pacman()],
+        format="Updates: {count}")
 
 status.register("disk",
         hints={"color":"#ffffff"},
--- i3/i3status-zentux.py
+++ i3/i3status-zentux.py
@@ -1,4 +1,5 @@
 from i3pystatus import Status
+from i3pystatus.updates import pacman
 
 """
 Python packet dependency:
@@ -10,11 +11,14 @@ psutil
 
 status = Status(standalone=True)
 
-status.register("clock", format=" %d %B %H:%M",hints={"color":"#ffffff"})
+status.register("clock",
+                format="%d %B %H:%M",
+                hints={"color":"#ffffff"},
+                on_leftclick="xdg-open https://calendar.google.com")
 
 status.register("battery",
-                hints={ "color":"#ffffff"},
-                format=" {status} {consumption:.0f}W {percentage:.2f}% {remaining:%E%hh:%Mm}",
+                hints={"color":"#ffffff"},
+                format="{status} {consumption:.0f}W {percentage:.2f}% {remaining:%E%hh:%Mm}",
                 alert_percentage=15,
                 charging_color="#F2FF00",
                 status={
@@ -24,40 +28,53 @@ status.register("battery",
                 })
 
 status.register("pulseaudio",
-                format="   {volume}% ",
-                hints={"color":"#ffffff"})
+                format="  {volume}%",
+                hints={"color":"#ffffff"},
+                on_upscroll="zenbook-audio.sh -v +",
+                on_downscroll="zenbook-audio.sh -v -",
+                on_leftclick="zenbook-audio.sh -m",
+                on_rightclick="pavucontrol")
 
-status.register("mem", format=" MEM {percent_used_mem}%")
+status.register("mem",
+                format="MEM {percent_used_mem}%",
+                on_leftclick="terminator --geometry=600x400 -x htop")
 
 status.register("cpu_usage",
-                format= "CPU {usage}%",
+                format="CPU {usage}%",
                 hints={ "color":        "#ffffff",
                         "full_text":    "CPU 4%",
                         "min_width":    "CPU 100%",
                         "align":        "center"
-                })
+                },
+                on_leftclick="terminator --geometry=600x400 -x htop")
 
 status.register("dpms",
-                format = " DPMS: {status} ",
+                format="DPMS: {status}",
                 hints={"color":"#ffffff"})
 
 status.register("network",
         interface="eth0",
-        format_up=" {v4cidr} ",
+        format_up="{v4cidr}",
         format_down="")
 
 status.register("network",
         interface="wlp2s0",
-        format_up=" {essid} ",
+        format_up="{essid}",
         interval=5)
 
+'''
 status.register("runwatch",
         path="/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/plexmediaserver.pid",
         name="Plex")
+'''
+
+status.register("updates",
+        backends = [pacman.Pacman()],
+        format="Updates: {count}")
 
 status.register("disk",
         hints={"color":"#ffffff"},
         path="/",
-        format=" root {percentage_used:.0f}% ")
+        format="root {percentage_used:.0f}%")
 
 status.run()
