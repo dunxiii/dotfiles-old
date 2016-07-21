0cf6654d114018d47de954d46c38d03c0d4f7054
Jack Söderblom <dunxiii@gmail.com>
2016-07-21 11:21:15 +0200

    Update

--- i3/create_config.sh
+++ i3/create_config.sh
@@ -10,7 +10,7 @@ echo -e "# This file is auto-generated, do not make changes in it!" >> "${CONFIG
 echo -e "# vim: set foldmethod=marker:" >> "${CONFIG}"
 
 for FILE in base startup keybinds windows bar mode; do
-    echo -e "\n# {{{ ${FILE}" >> "${CONFIG}"
+    echo -e "# {{{ ${FILE}" >> "${CONFIG}"
     "${HOME}/.i3/config.d/${FILE}" >> "${CONFIG}"
     echo -e "# }}}" >> "${CONFIG}"
 done
